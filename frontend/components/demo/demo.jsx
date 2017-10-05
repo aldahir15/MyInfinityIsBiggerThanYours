1462c040ea764027c10413f0fb72aeeec6cb8674
Jose Aldahir Moreno <jamoren1@uci.edu>
2017-10-04 20:48:20 -0700

    new forumla for diagonal circles

--- frontend/components/demo/demo.jsx
+++ frontend/components/demo/demo.jsx
@@ -27,11 +27,12 @@ class Demo extends React.Component {
       document.getElementsByClassName(`circle-${i}`)[0].style.borderColor = "red";
     }
     for (let i = 0; i < this.state.numbers.length; i++) {
-      const yaxis = (-30.9) + (3.3)*(i);
+      const xaxis = (-68.2) + (2.4)*(i);
+      const yaxis = (3.1) + (0.9)*(i);
       anime({
         targets: `.circle-${i}`,
         translateY: `${yaxis}rem`,
-        translateX: '5.6rem',
+        translateX: `${xaxis}rem`,
         scale: [.75, .9],
         delay: function(el, index) {
           return index * 80;
@@ -83,19 +84,21 @@ class Demo extends React.Component {
 
   render() {
     return (
-      <div className="wrap">
-        <div className="demo-button-div">
-          <a onClick={() => this.refs.child.handleRandom()}>Random Set</a>
-          <div className="demo-divider"></div>
-          <a onClick={this.handleReset}>Reset</a>
-          <div className="demo-divider"></div>
-          <a onClick={this.handleNewNumber}>Find a New Number!</a>
+      <div className="wrap-wrap">
+        <div className="wrap">
+          <div className="demo-button-div">
+            <a onClick={() => this.refs.child.handleRandom()}>Random Set</a>
+            <div className="demo-divider"></div>
+            <a onClick={this.handleReset}>Reset</a>
+            <div className="demo-divider"></div>
+            <a onClick={this.handleNewNumber}>Find a New Number!</a>
+          </div>
+          <div className="demo-container"><NumberSet ref="child" parent={this} /></div>
         </div>
-        <div className="demo-container"><NumberSet ref="child" parent={this} /></div>
         <div className="circle-container">
         {this.state.numbers.map((num) =>
           <div key={num} id="circle-demo" className={`circle-${this.state.numbers.indexOf(num)}`}></div>)}
-        </div>
+          </div>
       </div>
     );
   }
